8b9d390f87faee3918c613201ea362aa0fca0ad8
Adhityads <114919478+Adhityads@users.noreply.github.com>
2022-10-04 16:00:50 +0700

    Update Main.java

--- src/com/juaracoding/gitjava/Main.java
+++ src/com/juaracoding/gitjava/Main.java
@@ -5,5 +5,6 @@ public class Main {
         // GitHub
         System.out.println("Hello world!");
         System.out.println("Ini Tambahan Coding");
+        System.out.println("Ini dari mas Harun");
     }
 }
